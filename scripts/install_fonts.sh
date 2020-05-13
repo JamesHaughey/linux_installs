cca128e1e985f8da554f429e66f21fd2323f10a9
james <jimmyhaugh@googlemail.com>
2020-05-13 09:27:45 +0100

    initial commit

---
+++ scripts/install_fonts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+URL_BASE=https://github.com/romkatv/powerlevel10k-media/raw/master/
+FILE_NAMES=(
+    MesloLGS%20NF%20Regular.ttf
+    MesloLGS%20NF%20Bold.ttf
+    MesloLGS%20NF%20Italic.ttf
+    MesloLGS%20NF%20Bold%20Italic.ttf
+)
+FONT_FOLDER=~/.fonts/
+
+mkdir ${FONT_FOLDER}
+
+for file in "${FILE_NAMES[@]}"; do
+    wget -O ${FONT_FOLDER}${file} ${URL_BASE}${file}
+done
